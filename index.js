5d5a7269f9b8ef9dd4b90ec1af697ce54009322d
Jadeja Anirudh sinh <108729246+ANIRUDHSINH01@users.noreply.github.com>
2023-02-21 09:05:35 +0000

    New

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Plugins } = require ("./src/index.js")
+
+
+console.log('\033[36sshakti.js is trying to be packed!');
+
+
+console.log('\033[92mDiscord server : https://discord.gg/4Kx9A3QhC2'); setTimeout(() => { 
+  
+console.log('\033[32sshakti.js is now sucessfully packed!'); }, 1500);
+
+
+module.exports = {
+  Plugins
+};
