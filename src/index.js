cdcbff98e79a998cb92780a429aeae879e7702bd
Jadeja Anirudh sinh <108729246+ANIRUDHSINH01@users.noreply.github.com>
2023-07-20 10:39:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -65,6 +65,32 @@ bot.functionManager.createFunction({
 });
 
 bot.functionManager.createFunction({
+  name: `$isFileEmpty`,
+  type: "djs",
+  code: async d => {
+    const data = d.util.aoiFunc(d);
+    const [filePath] = data.inside.splits;
+
+    const isFileEmpty = filePath => {
+      try {
+        const stats = fs.statSync(filePath);
+        return stats.size === 0;
+      } catch (error) {
+        // Handle file not found or other errors
+        console.error('Error:', error);
+        return false;
+      }
+    }
+
+    data.result = isFileEmpty(filePath);
+
+    return {
+      code: d.util.setCode(data)
+    };
+  }
+});
+
+bot.functionManager.createFunction({
   name: "$emojiProgressBar",
   type: "djs",
   code: async (d) => {
